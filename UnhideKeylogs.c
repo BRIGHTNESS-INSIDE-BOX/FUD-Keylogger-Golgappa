0797a90a0c19e30dfaeba7ce1ca1aca11ec0490f
Brightness-Inside-Box <33171575+BRIGHTNESS-INSIDE-BOX@users.noreply.github.com>
2019-06-09 00:35:04 -0400

    Add files via upload
    After Compilation, You Must Execute This File In The Same Folder ,To Unhide The KeyLogs ,
    Or If You Have Made Any Changes You Must Specify The Path In The Source Code File.

---
+++ UnhideKeylogs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+
+
+  /* Make Sure To Run This File
+
+  In The Same Folder You have
+
+  executed the logger
+
+
+  or if You Have Made Any Changes To Path
+
+
+  Make Same Changes Here Also
+
+  */
+
+
+  system("attrib -h -s Pikachu.txt");
+
+
+}
